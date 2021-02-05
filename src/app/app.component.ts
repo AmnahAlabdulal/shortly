6a2adacb6610425b5393df4518a7da032ee6bac9
Amnah1991 <amnah.abdulal@gmail.com>
2021-02-05 14:15:04 +0300

    js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { toEditorSettings } from 'typescript';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shortly';
+  objLink = {
+    longLink: '',
+    shortLink: ''
+  }
+  objLinks = new Array();
+
+  constructor(private http: HttpClient) { }
+
+  getShortLinks(link: string) {
+    if (link.length != 0) {
+      this.objLink.longLink = link;
+      var data = this.http.get(`https://api.shrtco.de/v2/shorten?url=${this.objLink.longLink}`)
+        .subscribe(res => {
+          let data: any = res;
+          this.objLink.shortLink = data.result.short_link;
+          this.objLinks.push(this.objLink);
+          (<HTMLInputElement>document.getElementById('link')).value ='';
+        });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     NgbModule
   ],
   providers: [],
