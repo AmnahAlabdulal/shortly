d92ccaec9df0fa1fb466d09c8dba17a7b2bb702b
Amnah1991 <amnah.abdulal@gmail.com>
2021-02-07 14:39:28 +0300

    js + css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { toEditorSettings } from 'typescript';
+import { NgxSpinnerService } from "ngx-spinner";
+import { FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -11,21 +13,31 @@ export class AppComponent {
   title = 'shortly';
   objLinks = new Array();
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private spinner: NgxSpinnerService) {
   }
 
   getShortLinks(link: string) {
     if (link.length != 0) {
+      this.spinner.show();
       var data = this.http.get(`https://api.shrtco.de/v2/shorten?url=${link}`)
         .subscribe(res => {
           let data: any = res;
           this.objLinks.push({ long: link, short: data.result.short_link });
           (<HTMLInputElement>document.getElementById('link')).value = '';
+          this.spinner.hide();
         });
+    } else {
+       
     }
   }
 
-  copyShortLink(link: string) {
-    console.log(link);
+  copyShortLink(link: string, index: number) {
+    navigator.clipboard.writeText(link).then(function () {
+      var temp = document.querySelectorAll('.secondary')[index];
+      (<HTMLInputElement>temp).textContent = 'Copied';
+      (<HTMLInputElement>temp).style.backgroundColor = '#3b3054';
+    }, function (err) {
+      alert('Async: Could not copy text: ');
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { NgxSpinnerModule } from "ngx-spinner";
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+ 
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    NgxSpinnerModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
